1f06d6b571d18272f2b15ba917230ac3c96f322c
Cheng-Dar Chiang <tboydar@gmail.com>
2015-04-23 23:16:39 +0800

    測試版

--- arduino_led_pir/arduino_led_pir.ino
+++ arduino_led_pir/arduino_led_pir.ino
@@ -2,24 +2,19 @@
   PIR("Passive Infrared Sensor") Motion Sensor,
  紅外線動作感測器, 或稱人體紅外線感測器
  */
-
-
 //led
 
 int brightness = 0;
-int fadeAmount = 5;
-int delayDuration = 60;
-
-
-
+int fadeAmount = 5;          // 漸變程度
+int delayDuration = 60;      // 漸變間隔
 const int PIRSensor = 2;     // 紅外線動作感測器連接的腳位
 const int ledPin =  13;      // LED 腳位
 int sensorValue = 0;         // 紅外線動作感測器訊號變數
+
 void setup() {
   pinMode(PIRSensor, INPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(9, HIGH);
-
 }
 
 
@@ -35,17 +30,20 @@ void loop() {
     brightness = 0;
   }
 
+  //判斷感應狀況, true =>開始漸變
   if (sensorValue) {
-
     brightness = brightness + fadeAmount;
     if (brightness <= 0 || brightness >= 255) {
       fadeAmount = -fadeAmount ;
     }
+  } else {
+    brightness = 0;
   }
+  // 限制亮度數值在0~255
   brightness =  constrain(brightness, 0, 255);
-
+  // led 亮的程度
   analogWrite(9, brightness);
-
+  // 延遲時間
   delay(delayDuration);
 
 }
